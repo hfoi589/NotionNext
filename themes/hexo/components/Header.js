464c89d6d170279505f4d2141eb27695baa2bba2
Wilson Zeng <39638586+hfoi589@users.noreply.github.com>
2023-06-06 20:24:45 +0800

    Update Header.js

--- themes/hexo/components/Header.js
+++ themes/hexo/components/Header.js
@@ -108,12 +108,6 @@ return (
   </>
 );
 
-    <div className="text-white absolute bottom-0 flex flex-col h-full items-center justify-center w-full">
-      <div className='font-black text-4xl md:text-5xl shadow-text'>{siteInfo?.title}</div>
-      <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
-        <span id='typed' />
-      </div>
-
                 {/* 首页导航插件 */}
                 {CONFIG_HEXO.HOME_NAV_BUTTONS && <NavButtonGroup {...props} />}
 
